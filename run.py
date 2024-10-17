37b3be34eab6f534fa5fa5be672217daec70cb35
PhilippMaschi <phil.masch@hotmail.com>
2024-10-17 15:07:00 +0200

    segmenting plot images

--- run.py
+++ run.py
@@ -3,4 +3,11 @@ from solarnet.run import RunTask
 
 
 if __name__ == '__main__':
-    fire.Fire(RunTask)
+    
+    # RunTask().make_masks()
+    # RunTask().split_images()
+    # RunTask().train_classifier(retrain=True, warmup=2, max_epochs=100, patience=5)
+    # RunTask().train_both()
+    # RunTask().classify_new_data(retrained=True)
+    RunTask().segment_new_data()
+    # fire.Fire(RunTask)
