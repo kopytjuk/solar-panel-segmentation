bb9e128daaad64c7d93a2c48632e668ea01e0f8b
Marat Kopytjuk <kopytjuk@gmail.com>
2025-03-03 09:19:51 +0100

    Create poetry environment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import fire
-from solarnet.run import RunTask
 
+from solarnet.run import RunTask
 
 if __name__ == '__main__':
     
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     # RunTask().train_classifier(retrain=True, warmup=2, max_epochs=100, patience=5)
     # RunTask().train_both()
     # RunTask().classify_new_data(retrained=True)
-    RunTask().segment_new_data()
-    # fire.Fire(RunTask)
+    # RunTask().segment_new_data()
+    fire.Fire(RunTask)
